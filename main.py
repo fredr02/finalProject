f10ca2acc766ff458cd1d0a9552e24b9fd57f81d
Fred Lopez <flopez@easinc.net>
2021-12-01 15:02:09 -0500

    code reduction

--- main.py
+++ main.py
@@ -10,62 +10,28 @@ def mainMenu():
     mainFrame = LabelFrame(root, text="Main Menu", padx=30, pady=30)
     mainFrame.pack(pady=150)
 
-    btn_verifyISBN10 = Button(mainFrame, text="1. Verify the check digit of an ISBN-10",
-                              command=lambda: [mainFrame.forget(), verifyISBN10_Menu()]).grid(row=0, column=0)
-    btn_verifyISBN13 = Button(mainFrame, text="2. Verify the check digit of an ISBN-13",
-                              command=lambda: [mainFrame.forget(), verifyISBN13_Menu()]).grid(row=1, column=0)
-    btn_convertI10toI13 = Button(mainFrame, text="3. Convert an ISBN-10 to an ISBN-13",
-                                 command=lambda: [mainFrame.forget(), convertI10toI13_Menu()]).grid(row=2, column=0)
-    btn_convertI13toI10 = Button(mainFrame, text="4. Convert an ISBN-13 to an ISBN-10",
-                                 command=lambda: [mainFrame.forget(), convertI13toI10_Menu()]).grid(row=3, column=0)
+    menuNames = ["Verify the check digit of an ISBN-10", "Verify the check digit of an ISBN-13",
+                 "Convert an ISBN-10 to an ISBN-13", "Convert an ISBN-13 to an ISBN-10"]
+
+    for index, menu in enumerate(menuNames):
+        btn = Button(mainFrame, text=menuNames[index],
+                     command=lambda: [mainFrame.forget(), item_Menu(menu)]).grid(row=index, column=0)
 
 
-def verifyISBN10_Menu():
-    verifyISBN10_Frame = LabelFrame(
-        root, text="Verify ISBN 10", padx=30, pady=30)
-    verifyISBN10_Frame.pack()
+def item_Menu(itemName):
+    itemFrame = LabelFrame(
+        root, text=itemName, padx=30, pady=30)
+    itemFrame.pack()
 
-    openFile = Button(verifyISBN10_Frame, text="Open ISBN List",
-                      command=lambda: askopenfilename()).grid(row=0, column=0)
+    openFileButton = Button(itemFrame, text="Open ISBN List",
+                            command=lambda: askopenfilename()).grid(row=0, column=0)
 
-    back = Button(verifyISBN10_Frame, text="Back", command=lambda: [
-                  verifyISBN10_Frame.forget(), mainMenu()]).grid(row=1, column=0)
-
-
-def verifyISBN13_Menu():
-    verifyISBN13_Frame = LabelFrame(
-        root, text="Verify ISBN 13", padx=30, pady=30)
-    verifyISBN13_Frame.pack()
-
-    openFile = Button(verifyISBN13_Frame, text="Open ISBN List",
-                      command=lambda: askopenfilename()).grid(row=0, column=0)
-
-    back = Button(verifyISBN13_Frame, text="Back", command=lambda: [
-                  verifyISBN13_Frame.forget(), mainMenu()]).grid(row=1, column=0)
+    back = Button(itemFrame, text="Back", command=lambda: [
+        itemFrame.forget(), mainMenu()]).grid(row=1, column=0)
 
 
-def convertI10toI13_Menu():
-    convertI10toI13_Frame = LabelFrame(
-        root, text="Convert I10 to I13", padx=30, pady=30)
-    convertI10toI13_Frame.pack()
-
-    openFile = Button(convertI10toI13_Frame, text="Open ISBN List",
-                      command=lambda: askopenfilename()).grid(row=0, column=0)
-
-    back = Button(convertI10toI13_Frame, text="Back", command=lambda: [
-                  convertI10toI13_Frame.forget(), mainMenu()]).grid(row=1, column=0)
-
-
-def convertI13toI10_Menu():
-    convertI10toI10_Frame = LabelFrame(
-        root, text="Convert I13 to I10", padx=30, pady=30)
-    convertI10toI10_Frame.pack()
-
-    openFile = Button(convertI10toI10_Frame, text="Open ISBN List",
-                      command=lambda: askopenfilename()).grid(row=0, column=0)
-
-    back = Button(convertI10toI10_Frame, text="Back", command=lambda: [
-                  convertI10toI10_Frame.forget(), mainMenu()]).grid(row=1, column=0)
+def createFile():
+    print(open(askopenfilename()).read())
 
 
 mainMenu()
