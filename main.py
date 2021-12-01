9d8f4519027f8c1492969917774d5cbd992261e0
Fred Lopez <flopez@easinc.net>
2021-12-01 16:37:12 -0500

    Software works - Now needs isbn logic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askopenfilename, asksaveasfilename
 
 root = Tk()
 root.title("ISBN Tool")
@@ -27,20 +27,53 @@ def item_Menu(itemName):
     itemFrame.pack(pady=150)
 
     # Label to indicate Status
-    Label(itemFrame, text="Awaiting List", font=(
+    labelText = StringVar()
+    labelText.set("Awaiting List")
+    Label(itemFrame, textvariable=labelText, font=(
         "Arial", 20),).grid(row=0, column=0)
 
     # Button To Open File
     Button(itemFrame, text="Open ISBN List",
-           command=lambda: createFile(itemName)).grid(row=1, column=0, sticky=N)
+           command=lambda: openFile(itemName, labelText, itemFrame)).grid(row=1, column=0, sticky=N)
 
     # Button to go back
     Button(itemFrame, text="↩", width=1, command=lambda: [
         itemFrame.forget(), mainMenu()]).grid(row=4, column=0)
 
 
-def createFile(itemName):
-    print(open(askopenfilename()).read())
+def openFile(itemName, labelText, itemFrame):
+    file = open(askopenfilename())
+    saveFile = ""
+    for line in file:
+        if itemName == "Verify the check digit of an ISBN-10":
+            saveFile = saveFile + verifyISBN10(line)
+        elif itemName == "Verify the check digit of an ISBN-13":
+            saveFile = saveFile + verifyISBN13(line)
+        elif itemName == "Convert an ISBN-10 to an ISBN-13":
+            saveFile = saveFile + verifyISBN10(line)
+        elif itemName == "Convert an ISBN-13 to an ISBN-10":
+            saveFile = saveFile + verifyISBN10(line)
+    print(saveFile)
+
+    labelText.set("Ready to save!")
+    Button(itemFrame, text="Save File", command=lambda: [open(asksaveasfilename(), "w").write(saveFile), labelText.set("File Was Saved")]).grid(
+        row=2, column=0, sticky=N)
+
+
+def verifyISBN10(isbn):
+    return (isbn).rstrip("\n") + " Test\n"
+
+
+def verifyISBN13(isbn):
+    return isbn + "\n"
+
+
+def convert10to13(isbn):
+    return isbn + "\n"
+
+
+def convert13to10(isbn):
+    return isbn + "\n"
 
 
 mainMenu()
